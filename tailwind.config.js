c7858f7b3cccd82d3dedc42ae7749d9da625ed1e
groupfoursite <joshsimpas6@gmail.com>
2025-05-24 23:15:40 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./crud/templates/**/*.html",
+    "./crud/static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
